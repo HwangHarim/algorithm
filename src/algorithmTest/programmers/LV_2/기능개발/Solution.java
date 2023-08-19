b49c4959d0ebbe94aaf3c301de0a3a2743ceeb5c
HwangHarim <scopan01@naver.com>
2023-08-19 23:06:23 +0900

    Add: 프로그래머스 문제 추가

--- src/algorithmTest/programmers/LV_2/기능개발/Solution.java
+++ src/algorithmTest/programmers/LV_2/기능개발/Solution.java
@@ -2,38 +2,81 @@ package algorithmTest.programmers.LV_2.기능개발;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
 
 
 public class Solution {
 
     public static void main(String[] args) {
-
+        int[] one ={93, 30, 55};
+        int[] speeds ={1,30,5};
+        System.out.println(Arrays.toString(solution(one, speeds)));
     }
 
-    public int[] solution(int[] progresses, int[] speeds) {
-        Queue<Integer> result = new LinkedList<>();
-        List<Integer> answerList = new ArrayList<>();
+    public static int[] solution(int[] progresses, int[] speeds) {
+        ArrayList<Integer> answers = new ArrayList<>();
+        int[] answer;
+        int number=0;
+        int dayCount =1;
+        int finish=0;
+        Queue<Integer> q = new LinkedList<>();
 
-        for(int i=0; i<speeds.length; i++){
-            double reMain = (100 - progresses[i])/speeds[i];
-            int data = (int) Math.ceil(reMain);
-
-            if(!result.isEmpty()&&result.peek()<data){
-                answerList.add(result.size());
-                result.clear();
-            }
-            result.offer(data);
+        for(int progresse : progresses){
+            q.offer(progresse);
         }
-        answerList.add(result.size());
 
-        int[] answer = new int[answerList.size()];
-        for(int i=0;i<answer.length; i++) {
-            answer[i] = answerList.get(i);
+        int now = q.poll();
+
+        while(!q.isEmpty()){
+
+            now += speeds[number];
+            if(100 <= now){
+                now = q.poll();
+                finish++;
+                number++;
+                for(int i=number; i<progresses.length;i++){
+                    if(100 <= progresses[i]+dayCount*speeds[i]){
+                        finish++;
+                        number++;
+                        now = q.poll();
+                    }
+                    if(i == progresses.length-1){
+                        answers.add(finish);
+                        finish =0;
+                    }
+                }
+            }
+            dayCount++;
+        }
+
+        answer = new int[answers.size()];
+        for(int i =0; i<answers.size();i++){
+            answer[i] = answers.get(i);
         }
 
         return answer;
     }
 }
+/**
+ * Queue<Integer> result = new LinkedList<>();
+ *         List<Integer> answerList = new ArrayList<>();
+ *
+ *         for(int i=0; i<speeds.length; i++){
+ *             double reMain = (100 - progresses[i])/speeds[i];
+ *             int data = (int) Math.ceil(reMain);
+ *
+ *             if(!result.isEmpty()&&result.peek()<data){
+ *                 answerList.add(result.size());
+ *                 result.clear();
+ *             }
+ *             result.offer(data);
+ *         }
+ *         answerList.add(result.size());
+ *
+ *         int[] answer = new int[answerList.size()];
+ *         for(int i=0;i<answer.length; i++) {
+ *             answer[i] = answerList.get(i);
+ *         }
+ */
---
+++ src/algorithmTest/programmers/LV_2/땅따먹기/Solution.java
@@ -0,0 +1,36 @@
+package algorithmTest.programmers.LV_2.땅따먹기;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        int[][] init = {{2, 5, 1, 3}, {6, 8, 7, 5}, {4, 3, 2, 1}};
+
+        System.out.println(solution(init));
+    }
+
+    public static int solution(int[][] land) {
+        int answer = 0;
+
+        for (int i = 1; i < land.length; i++) {
+            land[i][0] += maxScore(land[i - 1][1], land[i - 1][2],
+                land[i - 1][3]); // 이전 행에서 현재칸을 제외한 칸의 최대값을 더함
+            land[i][1] += maxScore(land[i - 1][0], land[i - 1][2], land[i - 1][3]);
+            land[i][2] += maxScore(land[i - 1][0], land[i - 1][1], land[i - 1][3]);
+            land[i][3] += maxScore(land[i - 1][0], land[i - 1][1], land[i - 1][2]);
+        }
+
+        for (int score : land[land.length - 1]) { // 마지막 행의 최대값 탐색
+            answer = Math.max(answer, score);
+        }
+
+        return answer;
+    }
+
+    public static int maxScore(int a, int b, int c) {
+        int max = 0;
+
+        max = Math.max(Math.max(a, b), c);
+
+        return max;
+    }
+}
