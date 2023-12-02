6f5e6a34267e916fbb7e423aa5554a4397e23210
HwangHarim <scopan01@naver.com>
2023-12-02 23:59:18 +0900

    add : 프로그래머스 문제 추가

--- src/algorithmTest/programmers/LV_2/순열/Solution.java
+++ src/algorithmTest/programmers/LV_2/순열/Solution.java
@@ -6,4 +6,22 @@ public class Solution {
 
     }
 
+    static void permutation(int[] arr, int depth, int n, int r) {
+        if (depth == r) {
+            print(arr, r);
+            return;
+        }
+
+        for (int i=depth; i<n; i++) {
+            swap(arr, depth, i);
+            permutation(arr, depth + 1, n, r);
+            swap(arr, depth, i);
+        }
+    }
+
+    static void swap(int[] arr, int depth, int i) {
+        int temp = arr[depth];
+        arr[depth] = arr[i];
+        arr[i] = temp;
+    }
 }
